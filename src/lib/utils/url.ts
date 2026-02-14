25f3dad6fd8bebe640813a2bd0440a536ad99666
sheshiyer <sheshnarayan.iyer@gmail.com>
2026-02-14 07:09:10 +0530

    fix: prioritize window.location.origin for client side url generation

--- src/lib/utils/url.ts
+++ src/lib/utils/url.ts
@@ -4,6 +4,11 @@ export const getURL = () => {
         process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
         process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
         'http://localhost:3000/'
+
+    // If on client side and we have a window, prioritize that to ensure we match the current domain
+    if (typeof window !== 'undefined' && window.location.origin) {
+        url = window.location.origin
+    }
     // Make sure to include `https://` when not localhost.
     url = url.includes('http') ? url : `https://${url}`
     // Make sure to include a trailing slash
